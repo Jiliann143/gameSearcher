c4ccc34b6b04e1b38e11454ed06465739a5002b0
Julia <ynovikova@atomichronica.com>
2020-05-09 21:14:02 +0300

    Sort games by year with 2d array in collection view

--- GameSearcher/UI/MyGames/MyGamesController.swift
+++ GameSearcher/UI/MyGames/MyGamesController.swift
@@ -12,41 +12,79 @@ import Swiftools
 
 class MyGamesViewController: UIViewController {
     
-    private var dataSource: Results<GameItem>!
+    private var games: Results<GameItem>!
+    private var groupedGames = [[GameItem]]()
     
-    @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var collectionView: UICollectionView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        dataSource = RealmService.shared.objects(GameItem.self)
-        
-        tableViewSetup()
+        games = RealmService.shared.objects(GameItem.self)
+        assembleGroupedGames()
+        collectionViewSetup()
     }
     
-    private func tableViewSetup() {
-        tableView.registerCell(GameCell.self)
+    private func collectionViewSetup() {
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 16, bottom: 0, right: 16)
+        collectionView.registerCell(SimilarGamesCell.self)
+        collectionView.registerHeader(HeaderView.self)
+    }
+    
+    private func assembleGroupedGames() {
+           let groupedUsers = Dictionary(grouping: games) { $0.releaseYear }
+           let sortedKeys = groupedUsers.keys.sorted { $0 > $1 }
+           sortedKeys.forEach { (key) in
+               if let values = groupedUsers[key] {
+                   groupedGames.append(values)
+               }
+           }
+    }
+}
+
+
+extension MyGamesViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.cell(SimilarGamesCell.self, for: indexPath).setupGame(groupedGames[indexPath.section][indexPath.row])
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return groupedGames[section].count
     }
     
 }
 
+extension MyGamesViewController: UICollectionViewDelegate {
 
-extension MyGamesViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.cell(GameCell.self).setupGameInfo(dataSource[indexPath.row])
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        LogInfo(games[indexPath.item].gameInfo)
+        LogInfo(games[indexPath.item].releaseYear)
     }
 }
 
-extension MyGamesViewController: UITableViewDelegate {
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        LogInfo(dataSource[indexPath.row].gameInfo)
-        LogInfo(dataSource[indexPath.row].releaseYear)
-
+extension MyGamesViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 40)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let firstItemInSectionYear = groupedGames[indexPath.section].first!.releaseYear
+        return collectionView.header(HeaderView.self, for: indexPath).setup(String(firstItemInSectionYear))
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return groupedGames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3 - 10 - collectionView.contentInset.left
+        let height = width * 1.5
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        10
     }
 }
 
